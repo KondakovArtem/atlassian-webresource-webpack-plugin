2f576b7c5214cde09ea8f9f83b8644a66718bd30
Christopher Darroch <cdarroch@atlassian.com>
2017-04-10 22:33:09 +0200

    Disable output of dev-mode web-resources and web-resource conditions until the feature is re-implemented.
    The extraction from our apps assumes that there will be certain Java-based Condition implementations available in the product or plugin.
    This is not going to be the case for third party plugins, until such time as there is a common maven plugin that ships them.
    
    This feature will re-appear when this webpack plugin's API can be updated to allow pluggability for conditions in generated web-resources.

--- src/util/wrm-utils.js
+++ src/util/wrm-utils.js
@@ -30,7 +30,7 @@ function clientResourceTemplate(b) {
             return conditionsStr;
         }
     };
-    
+
     function getXml(template, arg1, arg2) {
         return arg1 ? template(arg1, arg2) : "";
     }
@@ -51,30 +51,11 @@ function clientResourceTemplate(b) {
 module.exports = {
 
     createResourceDescriptor(jsonDescriptor) {
-
-        jsonDescriptor.conditions = [];
-        
-        // by default we do not allow our web resources to bind to contexts when we are 
-        // unlicensed as this can add service desk resources to instances where it isn't
-        // even being used. There are some special cases where you may want to. These are
-        // declared in unlicensed-contexts.js
-        if (!_.includes(unlicensedContexts, jsonDescriptor.context)) {
-            jsonDescriptor.conditions.push({
-                condition: "com.atlassian.servicedesk.internal.conditions.urlreading.UrlReadingSDOperationalCondition"
-            });
+        // TODO: Introduce pluggability for web-resource conditions here.
+        // e.g., Allow for ServiceDesk to inject their licensed condition, or for a devmode hotreload server condition.
+        if (!jsonDescriptor.isDevModeOnly) {
+            return clientResourceTemplate(jsonDescriptor);
         }
-        if (jsonDescriptor.isDevModeOnly) {
-            jsonDescriptor.conditions.push({
-                condition: "com.atlassian.servicedesk.internal.conditions.WebpackDevModeUrlReadingCondition"
-            });
-        } else if (jsonDescriptor.isProdModeOnly) {
-            jsonDescriptor.conditions.push({
-                condition: "com.atlassian.servicedesk.internal.conditions.WebpackDevModeUrlReadingCondition",
-                invert: true
-            });
-        }
-
-        return clientResourceTemplate(jsonDescriptor);
     },
     createResourceDescriptors(jsonDescriptors) {
             const descriptors = jsonDescriptors.map((descriptor) => {
