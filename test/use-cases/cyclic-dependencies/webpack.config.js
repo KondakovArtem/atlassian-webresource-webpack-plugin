f69aa93b0d93dac91fa1e4f6dc93ead3be50055a
Felipe Reis <freis@atlassian.com>
2017-11-20 08:47:06 +1100

    Add tests to cyclic dependency handling

---
+++ test/use-cases/cyclic-dependencies/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const WrmPlugin = require("../../../src/index");
+const FRONTEND_SRC_DIR = path.resolve(__dirname, 'src');
+const OUTPUT_DIR = path.resolve(__dirname, 'target');
+
+module.exports = {
+    context: FRONTEND_SRC_DIR,
+    entry: {
+        'cyclic-entry': path.join(FRONTEND_SRC_DIR, 'root.js')
+    },
+    plugins: [
+        new WrmPlugin({
+            pluginKey: "com.atlassian.plugin.test",
+            contextMap: {"cyclic-entry": [""]},
+            xmlDescriptors: path.join(__dirname, 'target', 'META-INF', 'plugin-descriptor', 'wr-webpack-bundles.xml'),
+            verbose: false,
+        }),
+    ],
+    output: {
+        filename: '[name].js',
+        path: path.resolve(OUTPUT_DIR)
+    }
+};
