60c093753affa035a68218f85cfc094fe286a488
Tim Sebastian <tsebastian@atlassian.com>
2017-12-07 17:21:09 +1100

    extract main functionality into separate classes

--- src/index.js
+++ src/index.js
@@ -15,24 +15,23 @@
 
 const assert = require("assert");
 const fs = require("fs");
-const glob = require("glob");
 const path = require('path');
 const { createHash } = require('crypto');
-
+const PrettyData = require('pretty-data').pd;
 const uuidv4Gen = require('uuid/v4');
+
 const XMLFormatter = require("./XmlFormatter");
 const ProvidedExternalDependencyModule = require("./webpack-modules/ProvidedExternalDependencyModule");
 const WrmDependencyModule = require("./webpack-modules/WrmDependencyModule");
 const WrmResourceModule = require("./webpack-modules/WrmResourceModule");
-const baseContexts = require("./settings/base-contexts");
 const WRMHelpers = require("./WRMHelpers");
 const WebpackHelpers = require("./WebpackHelpers");
 const WebpackRuntimeHelpers = require("./WebpackRuntimeHelpers");
-const qUnitRequireMock = require("./shims/qunit-require-shim");
 const flattenReduce = require('./flattenReduce');
 const logger = require("./logger");
+const QUnitTestResources = require("./QUnitTestResources");
+const AppResources = require("./AppResources");
 
-const RESOURCE_JOINER = "__RESOURCE__JOINER__";
 class WrmPlugin {
 
     /**
@@ -74,7 +73,6 @@ class WrmPlugin {
         // generate an asset uuid per build - this is used to ensure we have a new "cache" for our assets per build.
         // As JIRA-Server does not "rebuild" too often, this can be considered reasonable.
         this.assetUUID = process.env.NODE_ENV === 'production' ? uuidv4Gen() : "DEV_PSEUDO_HASH";
-        this.qunitRequireMockPath = `qunit-require-shim-${this.assetUUID}.js`;
     }
 
     checkConfig(compiler) {
@@ -113,7 +111,8 @@ An automated jsonpFunction name for this plugin was created:
         });
     }
 
-    getTestFiles(context) {
+    getTestFiles() {
+        const context = this.options.context;
         const testGlobs = this.options.__testGlobs__;
 
         if(!testGlobs) {
@@ -227,131 +226,23 @@ ${standardScript}`
         // When the compiler is about to emit files, we jump in to produce our resource descriptors for the WRM.
         compiler.plugin("emit", (compilation, callback) => {
 
-            const qunitTestFiles = this.getTestFiles(compiler.options.context);
-
-            const assetFiles = Object.keys(compilation.assets)
-                    .filter(p => !/\.(js|css|soy)(\.map)?$/.test(p));
-
-            const assets = {
-                key: `assets-${this.assetUUID}`,
-                resources: assetFiles,
-            };
-
-            const entryPointNames = compilation.entrypoints;
-            const resourceToAssetMap = WebpackHelpers.extractResourceToAssetMapForCompilation(compilation.modules);
-
-            /**
-             * Every entrypoint has an attribute called "chunks".
-             * This contains all chunks that are needed to successfully "load" this entrypoint.
-             * Usually every entrypoint only contains one chunk - the bundle that is build for that entrypoint.
-             * If more than one chunk is present that means they are commons-chunks that contain code needed by the entrypoint to function.
-             * All chunks in the "chunks"-array are in the order they need to be loaded in - therefore the actual entrypoint is always the last in that array.
-             * Hence, if we find an entrypoint with more than one chunk, we can assume that every but the last chunk are commons chunks and have to be handled as such.
-             *
-             * IMPORTANT-NOTE: async-chunks required by this entrypoint are not specified in the entrypoint but as sub-chunks of the entrypoint chunk.
-             */
-            const commonsChunks = Array.from(Object.keys(entryPointNames)
-                .map(name => entryPointNames[name].chunks) // get chunks per entry
-                .filter(cs => cs.length > 1) // check if commons chunks exist
-                .map(cs => cs.slice(0, -1)) // only take all chunks up to the actual entry chunk
-                .reduce(flattenReduce, []) // flatten arrays
-                .reduce((set, c) => {
-                    set.add(c);
-                    return set;
-                }, new Set())); // deduplicate
-
-            /**
-             * Create a key and the fully-qualified web-resource descriptor for every commons-chunk.
-             * This is needed to point to reference these chunks as dependency in the entrypoint chunks
-             *
-             * <web-resource>
-             *   ...
-             *   <dependency>this-plugin-key:commons_some_chunk</dependency>
-             *   ...
-             */
-            const commonsChunkDependencyKeyMap = new Map();
-            for(const c of commonsChunks) {
-                commonsChunkDependencyKeyMap.set(c.name, {
-                    key: `commons_${c.name}`,
-                    dependency: `${this.options.pluginKey}:commons_${c.name}`
-                })
-            }
-
-            /**
-             * Create descriptors for the commons-chunk web-resources that have to be created.
-             * These include - like other chunk-descriptors their assets and external resources etc.
-             */
-            const commonDescriptors = commonsChunks.map(c => {
-                const additionalFileDeps = WebpackHelpers.getDependencyResourcesFromChunk(c, resourceToAssetMap);
-                return {
-                    key: commonsChunkDependencyKeyMap.get(c.name).key,
-                    externalResources: WebpackHelpers.getExternalResourcesForChunk(c),
-                    resources: Array.from(new Set(c.files.concat(additionalFileDeps))),
-                    dependencies: WebpackHelpers.getDependenciesForChunks([c])
-                }
-            });
-
-            // Used in prod
-            const prodEntryPoints = Object.keys(entryPointNames).map(name => {
-                const webresourceKey = WRMHelpers.getWebresourceKeyForEntry(name, this.options.webresourceKeyMap);
-                const entrypointChunks = entryPointNames[name].chunks;
-                const actualEntrypointChunk = entrypointChunks[entrypointChunks.length-1];
-
-                // Retrieve all commons-chunk this entrypoint depends on. These must be added as "<dependency>"s to the web-resource of this entrypoint
-                const commonDeps = entrypointChunks.map(c => commonsChunkDependencyKeyMap.get(c.name)).filter(Boolean).map(val => val.dependency);
-
-                const additionalFileDeps = entrypointChunks.map(c => WebpackHelpers.getDependencyResourcesFromChunk(c, resourceToAssetMap));
-                const extractedTestResources = Array.from(WebpackHelpers.extractAllFilesFromChunks([actualEntrypointChunk], compiler.options.context, RESOURCE_JOINER))
-                    .map(resource => {
-                        if (resource.includes(RESOURCE_JOINER)) {
-                            return resource.split(RESOURCE_JOINER);
-                        }
-                        return [resource, resource];
-                    });
-                const pathPrefix = WRMHelpers.extractPathPrefixForXml(compiler.options);
-                const testFiles = [
-                    [`${pathPrefix}${this.qunitRequireMockPath}`, `${pathPrefix}${this.qunitRequireMockPath}`] // require mock to allow imports like "wr-dependency!context"
-                ].concat(extractedTestResources);
-                const testDependencies = Array.from(WebpackHelpers.extractAllDependenciesFromChunk(entrypointChunks));
-                return {
-                    key: webresourceKey,
-                    contexts: WRMHelpers.getContextForEntry(name, this.options.contextMap),
-                    externalResources: WebpackHelpers.getExternalResourcesForChunk(actualEntrypointChunk),
-                    resources: Array.from(new Set([].concat(actualEntrypointChunk.files, ...additionalFileDeps))),
-                    dependencies: baseContexts.concat(WebpackHelpers.getDependenciesForChunks([actualEntrypointChunk]), commonDeps),
-                    conditions: WRMHelpers.getConditionForEntry(name, this.options.conditionMap),
-                    testFiles,
-                    testDependencies
-                };
-            });
-
-            const asyncChunkDescriptors = WebpackHelpers.getChunksWithEntrypointName(entryPointNames, compilation.chunks).map(c => {
-                const additionalFileDeps = WebpackHelpers.getDependencyResourcesFromChunk(c, resourceToAssetMap);
-                return {
-                    key: `${c.id}`,
-                    externalResources: WebpackHelpers.getExternalResourcesForChunk(c),
-                    resources: Array.from(new Set(c.files.concat(additionalFileDeps))),
-                    dependencies: WebpackHelpers.getDependenciesForChunks([c])
-                }
-            });
-
-            const wrmDescriptors = commonDescriptors
-                .concat(asyncChunkDescriptors)
-                .concat(prodEntryPoints)
-                .concat(assets);
-
+            
             const pathPrefix = WRMHelpers.extractPathPrefixForXml(compiler.options);
-            const xmlDescriptors = XMLFormatter.createResourceDescriptors(pathPrefix, wrmDescriptors, qunitTestFiles);
+            const appResourceGenerator = new AppResources(this.assetUUID, this.options, compiler, compilation);
+            const testResourcesGenerator = new QUnitTestResources(this.assetUUID, this.options, compiler, compilation);
+            
+            const resourceDescriptors = XMLFormatter.createResourceDescriptors(pathPrefix, appResourceGenerator.getResourceDescriptors());
+            
+            testResourcesGenerator.injectQUnitShim();
+            const testResourceDescriptors = XMLFormatter.createTestResourceDescriptors(testResourcesGenerator.createAllFileTestWebResources());
+            const qUnitTestResourceDescriptors = XMLFormatter.createQUnitResourceDescriptors(testResourcesGenerator.getTestFiles());
+
+            const xmlDescriptors = PrettyData.xml(`<bundles>${resourceDescriptors}${testResourceDescriptors}${qUnitTestResourceDescriptors}</bundles>`);
             const xmlDescriptorWebpackPath = path.relative(compiler.options.output.path, this.options.xmlDescriptors);
             compilation.assets[xmlDescriptorWebpackPath] = {
                 source: () => new Buffer(xmlDescriptors),
                 size: () => Buffer.byteLength(xmlDescriptors)
             };
-
-            compilation.assets[this.qunitRequireMockPath] = {
-                source: () => new Buffer(qUnitRequireMock),
-                size: () => Buffer.byteLength(qUnitRequireMock)
-            }
 
             callback();
         });
