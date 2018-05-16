d175bfca8056dd37e292d71716d607c287a08bb4
Tim Sebastian <tsebastian@atlassian.com>
2018-05-16 11:45:03 +0200

    ensure AJS.contextPath is available

--- src/settings/base-contexts.js
+++ src/settings/base-contexts.js
@@ -1,1 +1,5 @@
-module.exports = ['jira.webresources:almond', 'com.atlassian.plugins.atlassian-plugins-webresource-plugin:data'];
+module.exports = [
+    'jira.webresources:almond',
+    'com.atlassian.plugins.atlassian-plugins-webresource-plugin:data',
+    'com.atlassian.plugins.atlassian-plugins-webresource-plugin:context-path',
+];
