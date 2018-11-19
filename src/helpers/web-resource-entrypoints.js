d1c4656ad1f0d46e2337eb2e18feffbc953d6bf2
Christopher Darroch <cdarroch@atlassian.com>
2018-11-19 17:28:31 +0100

    refactor: reduce

--- src/helpers/web-resource-entrypoints.js
+++ src/helpers/web-resource-entrypoints.js
@@ -1,28 +1,4 @@
-const path = require('path');
-
-const logger = require('./logger');
-
-module.exports = class WRMHelpers {
-    static stringifyAttributes(attributes) {
-        if (!attributes) {
-            return '';
-        }
-
-        return (
-            ' ' +
-            Object.keys(attributes)
-                .map(key => `${key}="${attributes[key]}"`)
-                .join(' ')
-        );
-    }
-
-    static renderElement(name, attributes, children) {
-        if (!children) {
-            return `<${name}${attributes}/>`;
-        }
-        return `<${name}${attributes}>${children}</${name}>`;
-    }
-
+class Helpers {
     static getContextForEntry(entry, contextMap) {
         const contexts = [].concat(entry).concat(contextMap[entry]);
         const validContexts = contexts.filter(context => context && typeof context === 'string');
@@ -44,32 +20,10 @@ module.exports = class WRMHelpers {
     static getConditionForEntry(entry, conditionMap) {
         return conditionMap[entry];
     }
+}
 
-    static extractPathPrefixForXml(options) {
-        const outputPath = options.output.path;
-        // get everything "past" the /target/classes
-        const pathPrefix = outputPath.split(path.join('target', 'classes'))[1];
-        if (pathPrefix === '' || pathPrefix === '/') {
-            return '';
-        } else if (pathPrefix === undefined) {
-            logger.warn(`
-******************************************************************************
-Path prefix for resources could not be extracted as the output path specified 
-in webpack does not point to somewhere in "target/classes". 
-This is likely to cause problems, please check your settings!
-
-Not adding any path prefix - WRM will probably not be able to find your files!
-******************************************************************************
-`);
-            return '';
-        }
-
-        // remove leading/trailing path separator
-        const withoutLeadingTrailingSeparators = pathPrefix.replace(
-            new RegExp(`^\\${path.sep}|\\${path.sep}$`, 'g'),
-            ''
-        );
-        // readd trailing slash - this time OS independent always a "/"
-        return withoutLeadingTrailingSeparators + '/';
-    }
+module.exports = {
+    getContextForEntry: Helpers.getContextForEntry,
+    getConditionForEntry: Helpers.getConditionForEntry,
+    getWebresourceAttributesForEntry: Helpers.getWebresourceAttributesForEntry,
 };
