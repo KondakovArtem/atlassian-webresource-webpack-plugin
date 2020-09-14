ad37fa1a70884dd434322d06e8e5b4ed4cd57ae9
Christopher Darroch <cdarroch@atlassian.com>
2020-09-14 17:36:09 +0200

    fix: clean output dirs before tests

--- test/hooks.js
+++ test/hooks.js
@@ -2,14 +2,20 @@ const chai = require('chai');
 const chaiXml = require('chai-xml');
 const chaiString = require('chai-string');
 const chaiUuid = require('chai-uuid');
+const del = require('del');
+
+function cleanOutputDirs() {
+    return del('test/**/target');
+}
 
 module.exports = {
     mochaHooks: {
-        beforeAll: function () {
+        beforeAll: function (done) {
             // global setup for all tests
             chai.use(chaiXml);
             chai.use(chaiString);
             chai.use(chaiUuid);
+            cleanOutputDirs().then(() => done());
         },
     },
 };
